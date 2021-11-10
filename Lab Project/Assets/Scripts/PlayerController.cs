71f4da9f01b8745cd6aadeacea9d4fa541cb8a62
Hide Ziegler <hidez246@gmail.com>
2021-11-10 11:04:24 -0700

    added player movement

--- Lab Project/Assets/Scripts/PlayerController.cs
+++ Lab Project/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float verticalInput;
+    private float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
     }
 }
