4e062c7fed8b97cf4734f222b783a61724f41b82
Hide Ziegler <hidez246@gmail.com>
2021-11-10 11:27:07 -0700

    added player constraints, organized code

--- Lab Project/Assets/Scripts/PlayerController.cs
+++ Lab Project/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private float speed = 10.0f;
     private float verticalInput;
     private float horizontalInput;
+    private float xBound = 11.0f;
+    private float zBound = 7.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +19,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+        PlayerConstraint();
+    }
+
+    // Player's movement
+    void PlayerMovement()
+    {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+
+    // Constrains player position
+    void PlayerConstraint()
+    {
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
     }
 }
