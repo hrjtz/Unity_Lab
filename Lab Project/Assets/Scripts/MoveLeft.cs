debd8c2b1bbf30cc76f17a63eae57beff437f3f7
Hide Ziegler <hidez246@gmail.com>
2021-11-29 20:43:08 -0700

    created MoveLeft script

---
+++ Lab Project/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed;
+    public float leftBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        destroyOutside();
+    }
+
+    void destroyOutside()
+    {
+        if (transform.position.x < leftBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
